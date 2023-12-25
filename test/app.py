ef61adc458e1afcebbd8c1eda228d466076e9a79
s8591120 <85890644+s8591120@users.noreply.github.com>
2023-12-25 20:39:47 +0800

    updata

--- test/app.py
+++ test/app.py
@@ -1,32 +1,76 @@
 from flask import Flask, render_template, jsonify, request
 import qrcode
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+import secrets
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+db = SQLAlchemy(app)
 
-menu = [
-    {"id": 1, "name": "Item 1", "price": 10.0},
-    {"id": 2, "name": "Item 2", "price": 15.0},
-    {"id": 3, "name": "Item 3", "price": 20.0},
-]
+class MenuItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class UserOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+
+
+admin = Admin(app, name='Order System', template_mode='bootstrap3')
+admin.add_view(ModelView(MenuItem, db.session))
+
+@app.route('/get_menu')
+def get_menu():
+    menu_items = MenuItem.query.all()
+    menu_data = [{'id': item.id, 'name': item.name, 'price': item.price} for item in menu_items]
+    return jsonify(menu_data)
 
 @app.route('/')
 def index():
+    menu = get_menu()
     return render_template('index.html', menu=menu)
 
 @app.route('/order', methods=['POST'])
 def order():
-    data = request.get_json()
-    selected_items = data.get('selectedItems', [])
-    total_price = sum(item['price'] for item in menu if item['id'] in selected_items)
-    return jsonify({"total_price": total_price})
+    try:
+        data = request.get_json()
+        selected_items = data.get('selectedItems', [])
+        menu_data = get_menu().json
+        total_price = sum(item['price'] for item in menu_data if item['id'] in selected_items)
+
+        # 將訂單寫入資料庫
+        for item_id in selected_items:
+            order = UserOrder(item_id=item_id, quantity=1, total_price=total_price)
+            db.session.add(order)
+        
+        db.session.commit()
+
+        print("Order written to database successfully!")
+
+        return jsonify({"total_price": total_price})
+    except Exception as e:
+        print(f"Error processing order: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
+
+
 
 @app.route('/qrcode')
 def generate_qrcode():
+    menu = get_menu()
     url = request.url_root.rstrip('/')  # 取得應用程式的根網址
     img = qrcode.make(url)
     img.save(os.path.join(app.static_folder, 'qrcode.png'))  # 儲存 QR Code 圖片
     return render_template('qrcode.html')
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.2.24', port=8080)
--- test/static/script.js
+++ test/static/script.js
@@ -1,22 +1,64 @@
-function calculateTotal() {
-    const checkboxes = document.querySelectorAll('.item-checkbox');
-    const selectedItems = Array.from(checkboxes)
-        .filter(checkbox => checkbox.checked)
-        .map(checkbox => parseInt(checkbox.value));
+// orderSystem.js
 
-    fetch('/order', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ selectedItems: selectedItems }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('totalPrice').innerText = `總價: $${data.total_price.toFixed(2)}`;
+// 取得菜單
+function getMenu() {
+    $.get('/get_menu', function(data) {
+        // 在這裡處理菜單項目數據
+        const menuList = $('#menu-list');
+        menuList.empty();
+        data.forEach(item => {
+            // 在每個菜單項目後添加一個 checkbox 供用戶選擇
+            menuList.append(`
+                <li>
+                    <input type="checkbox" name="menu-item" value="${item.id}">
+                    ${item.name} - $${item.price}
+                </li>
+            `);
+        });
     });
 }
 
-function submitOrder() {
-    alert('訂單已提交！');
+// 將訂單信息發送到後台
+function sendOrderToBackend(orderData) {
+    $.ajax({
+        type: 'POST',
+        url: '/order',
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({ selectedItems: orderData }),
+        success: function(response) {
+            console.log(response);
+            alert('Order completed successfully! Total Price: $' + response.total_price);
+            // 可以在這裡進行其他處理，例如刷新頁面或跳轉
+        },
+        error: function(error) {
+            console.error(error);
+            alert('Failed to complete order. Please try again.');
+        }
+    });
 }
+
+// 將訂單項目顯示在頁面上
+function displayOrder(order) {
+    const orderList = $('#order-list');
+    orderList.empty();
+    order.forEach(item => {
+        orderList.append(`<li>${item.name} - $${item.price}</li>`);
+    });
+}
+
+// 頁面加載完成後執行
+$(document).ready(function() {
+    getMenu(); // 取得菜單
+
+    // 完成訂單按鈕點擊事件
+    $('#complete-order').click(function() {
+        // 在這裡獲取用戶選擇的訂單項目
+        const selectedItems = [];
+        $('input[name="menu-item"]:checked').each(function() {
+            selectedItems.push(parseInt($(this).val()));
+        });
+
+        // 將訂單數據發送到後台
+        sendOrderToBackend(selectedItems);
+    });
+});
